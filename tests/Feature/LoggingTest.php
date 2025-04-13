a1e1b1d0a73b730b8e2ca060a7803566d1f0b635
Hanif Aulia Kusuma <hanifak44@gmail.com>
2025-04-13 20:59:43 +0700

    laravel logging selesai

--- tests/Feature/LoggingTest.php
+++ tests/Feature/LoggingTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class LoggingTest extends TestCase
@@ -18,5 +19,52 @@ class LoggingTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_logging()
+    {
+        Log::info("Hello Info");
+        Log::warning("Hello Warning");
+        Log::error("Hello Error");
+        Log::critical("Hello Critical");
+
+        self::assertTrue(true);
+    }
+
+    public function test_log_context()
+    {
+        Log::error("tes context", ['Name' => "Hanif"]);
+        self::assertTrue(true);
+    }
+
+    public function test_log_with_context()
+    {
+        Log::withContext(['Name' => 'Hanif']);
+        Log::error("error 1");
+        Log::critical("error 2");
+
+        self::assertTrue(true);
+    }
+
+    public function test_channel()
+    {
+        $stackLogger = Log::channel('stack');
+        $stackLogger->error("Hello Slack");
+
+        Log::error("jos banget");
+
+        self::assertTrue(true);
+    }
+
+    public function test_handler()
+    {
+        $handlerLogger = Log::channel('file');
+        $handlerLogger->debug("Hello Slack");
+        $handlerLogger->info("Hello Info");
+        $handlerLogger->warning("Hello Warning");
+        $handlerLogger->error("Hello Error");
+        $handlerLogger->critical("Hello Critical");
+
+        self::assertTrue(true);
     }
 }
